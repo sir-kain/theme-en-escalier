264c95404c3159d0f55fe40462971c6e6d649cf0
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-01-27 09:55:25 +0000

    redimensionnement de l'image de fond

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+
+
+  // Display other languages on click
+  $("nav li.active a").on('click', function(e) {
+    e.preventDefault();
+    $('nav li:not(.active)').slideToggle(400);
+    // $(this).closest('li').siblings('li').slideToggle(400);
+  });
+
+});
