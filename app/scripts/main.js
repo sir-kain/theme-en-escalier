eb712c2912f408b7b2732ee87dc1a350efacd1bf
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-01-27 12:31:08 +0000

    ajout du pied de page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
   // Display other languages on click
-  $("nav li.active a").on('click', function(e) {
+  $("nav li.active").on('click', function(e) {
     e.preventDefault();
     $('nav li:not(.active)').slideToggle(400);
     // $(this).closest('li').siblings('li').slideToggle(400);
