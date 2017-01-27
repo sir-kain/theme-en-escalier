f8202f29dfde31aaff2bcb50a025f4b583c8e083
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-01-27 15:36:21 +0000

    ajout du footer et d'un fichier responsive.scss

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+    // $('.loader').fadeOut(3000, function() {
+    //     $("#corps").fadeIn(1000);
+    // });
+
 
   // Display other languages on click
   $("nav li.active").on('click', function(e) {
