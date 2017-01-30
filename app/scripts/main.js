55c4b4e8b8441b8e232e930269a53f5bb5447150
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-01-30 21:10:06 +0000

    creation d'un nouveau navbar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // $('.loader').fadeOut(3000, function() {
-    //     $("#corps").fadeIn(1000);
-    // });
+  // $('.loader').fadeOut(3000, function() {
+  //     $("#corps").fadeIn(1000);
+  // });
 
 
   // Display other languages on click
-  $("nav li.active").on('click', function(e) {
+  $('nav li.active').on('click', function (e) {
     e.preventDefault();
-    $('nav li:not(.active)').slideToggle(400);
-    // $(this).closest('li').siblings('li').slideToggle(400);
+    $('nav li.active ~ li:not(.active)').fadeOut();
+    // $('nav li.active:active ~ li:not(.active)').slideToggle(400);
+    $(this).closest('li').siblings('li').slideToggle(400);
+  });
+
+  $(document).scroll(function () {
+    if ($(document).scrollTop() > $('.menu').height()) {
+
+      $('.menu').css({
+        'display': 'none'
+      });
+      $('.top_header').css({
+        'display': 'block'
+      });
+      setTimeout(function () {
+        $('.flag').removeClass('bounce');
+      }, 1000);
+    } else {
+      $('.menu').css({
+        'display': ''
+      });
+      $('.top_header').css({
+        'display': 'none'
+      });
+      setTimeout(function () {
+        $('.flag').addClass('bounce');
+      }, 1000);
+    }
+    // console.log('ok', $(document).scrollTop(), 'ko ', $('.menu').height());
   });
 
 });
